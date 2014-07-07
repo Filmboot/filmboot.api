e2a73e5bec1e16577c41de016f1dd9da08172bc6
benatespina <benatespina@gmail.com>
2014-07-07 17:58:07 +0200

    Updated composer, favicon and some files' license text

--- src/Myclapboard/MovieBundle/Model/Genre.php
+++ src/Myclapboard/MovieBundle/Model/Genre.php
@@ -1,8 +1,9 @@
 <?php
+
 /**
  * (c) benatespina <benatespina@gmail.com>
  *
- * This file belongs to Myclapboard.
+ * This file belongs to myClapboard.
  * The source code of application includes a LICENSE file
  * with all information about license.
  */
