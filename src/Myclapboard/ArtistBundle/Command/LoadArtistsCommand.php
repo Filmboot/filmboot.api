3e6c854b0cdbf70b756a0427347eec60710e3f8b
benatespina <benatespina@gmail.com>
2014-10-04 17:44:19 +0200

    Updated licenses and readme

--- src/Myclapboard/ArtistBundle/Command/LoadArtistsCommand.php
+++ src/Myclapboard/ArtistBundle/Command/LoadArtistsCommand.php
@@ -54,7 +54,7 @@ class LoadArtistsCommand extends DataFixtureCommand
     protected function hydrateFixture(ContainerInterface $container, ObjectManager $manager, $values)
     {
         $artist = $container->get('myclapboard_artist.manager.artist')->create();
-        $birthplace = $container->get('doctrine')->getRepository('JJsGeonamesBundle:City')
+        $birthplace = $manager->getRepository('JJsGeonamesBundle:City')
             ->findOneBy(array('geonameIdentifier' => $values['birthplace']));
 
         $artist->setFirstName($values['firstName']);
